993046240d9fe720c8d499be8e2c8f3b15ae2eda
yunyusha <576462286@qq.com>
2018-09-07 18:02:28 +0800

    xiawu

--- copy_hzvis/js/js.js
+++ copy_hzvis/js/js.js
@@ -1,15 +1,71 @@
 Vue.component('link-img',{
     data:function () {
         return{
-            style_img:{},
-            style_div:{}
+            style_a:{
+              display:'block',
+              width:'33.3%',
+              background:'red',
+              fontSize:'0',
+              float:'left',
+              overflow:'hidden',
+              position:'relative',
+              left:'0',
+              top:'0'
+            },
+            style_img:{
+                width:'100%',
+                height:'100%',
+                positive:'absolute',
+                left:'0',
+                top:'0',
+            },
+            style_div:{
+                display:'display',
+                opacity:'0',
+                background:'black',
+                position:'absolute',
+                left:'0',
+                top:'0',
+                width:'100%'
+            }
         }
     },
     props:['datalist'],
-    template:`<a href="#" :stlye="style_img" v-for="item in datalist"> 
-                <div :style_div></div>
-                <img src="" alt="">
-               </a>`
+    template:`<a href="#" :style="style_a" @mouseenter="entera" @mouseleave="leavea"> 
+                <div class="heimu" :style="style_div"></div>
+                <img :src="datalist.url" :style="style_img" alt="">
+               </a>`,
+    methods: {
+        entera: function (event) {
+            // alert('1');
+            //获取鼠标所在a标签的index值
+            let s = $(event.target).index();
+            // alert(s);
+            s++;
+            //获取图片当前高度
+            let l = $(`a:nth-child(${s}) img`).width();
+            let h = $(`a:nth-child(${s}) img`).height();
+            console.log(l);
+             $(`a:nth-child(${s}) img`).stop().animate({
+                width:'120%',
+                 height:'120%',
+             });
+            $(`a:nth-child(${s}) .heimu`).stop().animate({
+                opacity:'0.5'
+            });
+        },
+        leavea: function (event) {
+            let s = $(event.target).index();
+            s++;
+            $(`a:nth-child(${s}) img`).stop().animate({
+                width:'100%',
+                height:'100%'
+             });
+            $(`a:nth-child(${s}) .heimu`).stop().animate({
+                opacity:'0'
+            });
+        }
+    }
 });
 
 //Vue部分
@@ -29,7 +85,6 @@ let vm1 =new Vue({
             {'url':'images/9.jpg'},
             {'url':'images/10.jpg'}
         ]
-
     },
     methods: {
         enterl: function (event) {
@@ -71,6 +126,42 @@ let vm1 =new Vue({
     }
 });
 
+let vm2 = new Vue({
+    el:'#root2',
+    data:{
+        p0:"-",
+         action:[
+            {'url':'img/01.jpg','tit':'古茗茶饮',
+                'p1':'古茗茶饮品牌升级设计',
+                'p2':'品牌设计、IP设计、VI设计、品牌升级'
+            },
+            {'url':'img/02.jpg','tit':'每日瑜伽',
+                'p1':'每日瑜伽品牌设计',
+                'p2':'品牌设计、VI设计、吉祥物设计、IP设计'
+            },
+            {'url':'img/03.jpg','tit':'蓝小熊蓝莓',
+                'p1':'蓝小熊品牌策略与设计',
+                'p2':'品牌策略、品牌命名、品牌设计、VI设计、产品包装设计、网站设计'
+            },
+            {'url':'img/04.jpg','tit':'英树护肤品牌',
+                'p1':'英树品牌全案设计',
+                'p2':'品牌策划、品牌设计、VI设计、SI专卖店设计'
+            },
+            {'url':'img/05.gif','tit':'斑马自动售货机',
+                'p1':'斑马头策略设计',
+                'p2':'品牌策略、品牌命名、品牌设计、VI设计、网站设计'
+            },
+            {'url':'img/06.jpg','tit':';老娘舅餐饮',
+                'p1':'老娘舅品牌形象升级',
+                'p2':'品牌设计、VI设计、专卖店空间设计'
+            },
+            {'url':'img/07.jpg','tit':'甘兔庵抹茶',
+                'p1':'甘兔庵品牌全案策划设计',
+                'p2':'品牌策划、品牌设计、标识IP设计、专卖店设计'
+            }
+        ]
+    }
+});
 
 // Swiper部分
 
